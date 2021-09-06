2b370e663bf413d690d78855993dadc575edd4a9
Kollar Michall <kollar.michal21.gmail.com>
2021-09-06 12:19:48 +0200

    sho reducer

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -5,56 +5,9 @@ import { selectDictionarySections } from '../../redux/directory/directory.select
 import MenuItem from '../menu-item/MenuItem'
 import './directory.styles.scss'
 
-const data =  [ {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    linkUrl: 'hats'
 
-},
-{
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    linkUrl: ''
-
-
-},
-{
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    linkUrl: ''
-
-
-},
-{
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    size: 'large',
-    linkUrl: ''
-
-
-},
-{
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    size: 'large',
-    linkUrl: ''
-
-
-},]
 
 const Directory = ({ sections }) => {
-    // const [sections, setSections] = useState([])
-
-    // useEffect(() => {
-    //     setSections(data)
-    // }, [])
-
-
     return (
         <div className='directory-menu'>
             {sections.map(({ id, ...otherSectionProps}) => (
